8c2290b55634cb1ed4160228e05ec593ac2d1409
Aamir Raza <merciful9795@gmail.com>
2022-03-02 23:31:57 +0530

    second commit

--- resume/settings.py
+++ resume/settings.py
@@ -22,7 +22,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@a4eij(8rd(ulpd2tgdb3jstoa_^5yz%5^rriqj2)$p+arzoxw'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -128,4 +128,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
  
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = BASE_DIR / 'static/'
