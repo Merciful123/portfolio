977c51a552e818fdb3dd518fd6d811da57ec2d2f
Aamir Raza <merciful9795@gmail.com>
2022-03-03 21:50:04 +0530

    first commit

--- resume/resume/settings.py
+++ resume/resume/settings.py
@@ -22,12 +22,12 @@ TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@a4eij(8rd(ulpd2tgdb3jstoa_^5yz%5^rriqj2)$p+arzoxw'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["portfolio570.herokuapp.com", 'localhost']
 
  
 
@@ -128,4 +128,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
  
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = BASE_DIR / 'static/'
